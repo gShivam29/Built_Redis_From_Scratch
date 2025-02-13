86fd2b2e5df269d55d5a6369814569d44b0ed3fc
gShivam29 <shivamsgupta29@gmail.com>
2025-02-13 08:38:19 +0000

    added polling

--- 06_server.cpp
+++ 06_server.cpp
@@ -54,6 +54,48 @@ static void fd_set_nb(int fd){
 
 }
 
+static void conn_put(std::vector<Conn*> &fd2conn, struct Conn *conn){
+    if (fd2conn.size() <= (size_t) conn ->fd){
+        fd2conn.resize(conn -> fd + 1);
+    }
+    fd2conn[conn -> fd] = conn;
+}
+
+static int32_t accept_new_conn(std::vector<Conn*> &fd2conn, int fd){
+    struct sockaddr_in client_addr = {};
+    socklen_t socklen = sizeof(client_addr);
+    int connfd = accept(fd, (struct sockaddr *) &client_addr, &socklen);
+    if (connfd < 0){
+        die("accept() error");
+        return -1;
+    }
+
+    fd_set_nb(connfd);
+
+    struct Conn* conn = (struct Conn*)malloc(sizeof(struct Conn));
+    if (!conn){
+        close(connfd);
+        return -1;
+    }
+    conn -> fd = connfd;
+    conn -> state = STATE_REQ;
+    conn -> rbuf_size = 0;
+    conn -> wbuf_size = 0;
+    conn -> wbuf_sent = 0;
+    conn_put(fd2conn, conn);
+    return 0;
+}
+
+static void connection_io(Conn* conn){
+    if (conn -> state == STATE_REQ){
+        state_req(conn);
+    } else if (conn -> state == STATE_RES){
+        state_res(conn);
+    } else {
+        assert(0);
+    }
+}
+
 int main(){
 
     int fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -63,11 +105,14 @@ int main(){
         
 
     }
-    std::vector<Conn*> fd2conn;
+
+    std::vector<Conn*> fd2conn; //vector of connections
+
     fd_set_nb(fd);
 
-    std::vector<struct pollfd> poll_args;
+    std::vector<struct pollfd> poll_args; //vector of struct pollfd for poll arguments 
 
+ //event loop
     while(true){
         poll_args.clear();
         struct pollfd pfd = {fd,POLLIN,0};
@@ -87,7 +132,23 @@ int main(){
         int rv = poll(poll_args.data(), (nfds_t)poll_args.size(),1000);
         if (rv < 0){
             die("poll");
-        }2
+        }
+
+        for (size_t i =1; i < poll_args.size(); ++i){
+            if (poll_args[i].revents){
+                Conn* conn = fd2conn[poll_args[i].fd];
+                connection_io(conn);
+                if (conn -> state == STATE_END){
+                    fd2conn[conn -> fd] = NULL;
+                    (void) close (conn ->fd);
+                    free(conn);
+                }
+            }
+        }
+
+        if (poll_args[0].revents){
+            (void)accept_new_conn(fd2conn,fd);
+        }
     }
     return 0;
 }
